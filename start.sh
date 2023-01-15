ac668a646e1fffeba18840f9b6415860c0a88f07
prakashstar <prakashg022@gmail.com>
2023-01-15 18:07:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/prakashstar/bot.git /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
+git clone https://github.com/prakashstar/bot.git /bot
+cd /bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
